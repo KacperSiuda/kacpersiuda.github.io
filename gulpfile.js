c80c692e4bbb859c0c32758aba17f4b549dfc566
dzunik91 <mikolaj.junikowski@gmail.com>
2016-08-18 23:25:16 +0200

    browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
   return gulp.src('scss/**/*.scss')
     .pipe(sass()) 
     .pipe(gulp.dest('styles'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.watch('app/scss/**/*.scss', ['sass']); 
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+});
